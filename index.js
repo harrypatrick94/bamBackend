ca06ebb0f37b3876d338b048b58b1f31ff601f61
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-16 16:01:59 +1000

    test local deployment

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
+
+app.get('/test', (req, res) => {
+  res.send('hello world')
+})
 app.listen(3000, () => console.log('Server running on port 3000'))

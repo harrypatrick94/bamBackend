5f73faa87a6a81f76761140ed307df5cf9f2012b
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-17 08:42:52 +1000

    added email functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let express = require('express');
+const app = express()
 let router = express.Router();
 let nodemailer = require('nodemailer');
 let cors = require('cors');
@@ -8,7 +9,7 @@ const bcrypt = require('bcryptjs');
 let bodyParser = require('body-parser');
 const creds = require('./config/config.js');
 const PORT = 3000
-const app = express()
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json())

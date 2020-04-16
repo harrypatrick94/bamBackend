0a773d62a683ed6c818ea419a7dafe116047d3f0
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-17 07:30:24 +1000

    added dependencies to index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const app = express()
+let express = require('express');
+let router = express.Router();
+let nodemailer = require('nodemailer');
+let cors = require('cors');
+let mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+let bodyParser = require('body-parser');
+const creds = require('./config/config.js');
+
 app.get('/', (req, res) => {
   res.send('HEY!')
 })

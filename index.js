fc10a3feb8d82d1095a229f85a3b6041b6a32d2c
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-17 07:32:08 +1000

    added email functionality

--- index.js
+++ index.js
@@ -8,6 +8,51 @@ const bcrypt = require('bcryptjs');
 let bodyParser = require('body-parser');
 const creds = require('./config/config.js');
 
+let transport = {
+    host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
+    port: 587,
+    auth: {
+    user: creds.user,
+    pass: creds.pass
+  }
+}
+
+let transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+router.post('/send', (req, res, next) => {
+  console.log(req.query);
+  let name = req.body.name
+  let email = req.body.email
+  let message = req.body.message
+  let content = `name: ${name} \n email: ${email} \n message: ${message} `
+
+  let mail = {
+    from: name,
+    to: 'bensonandthemooch@gmail.com',  // Change to email address that you want to receive messages on
+    subject: 'New Message from Contact Form',
+    text: content
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
+    }
+  })
+})
 app.get('/', (req, res) => {
   res.send('HEY!')
 })

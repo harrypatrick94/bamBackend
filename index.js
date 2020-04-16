7314f4876cc3a87fe29dda9d9dec6960a54f3fd6
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-17 07:44:37 +1000

    bad gateway test

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const bcrypt = require('bcryptjs');
 let bodyParser = require('body-parser');
 const creds = require('./config/config.js');
 const PORT = 3000
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.json())
+app.use('/', router)
+
 let transport = {
     host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
     port: 587,
@@ -17,77 +23,7 @@ let transport = {
   }
 }
 
-let transporter = nodemailer.createTransport(transport)
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take messages');
-  }
-});
-
-router.post('/send', (req, res, next) => {
-  console.log(req.query);
-  let name = req.body.name
-  let email = req.body.email
-  let message = req.body.message
-  let content = `name: ${name} \n email: ${email} \n message: ${message} `
-
-  let mail = {
-    from: name,
-    to: 'bensonandthemooch@gmail.com',  // Change to email address that you want to receive messages on
-    subject: 'New Message from Contact Form',
-    text: content
-  }
-
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        status: 'fail'
-      })
-    } else {
-      res.json({
-       status: 'success'
-      })
-    }
-  })
-})
-
-// mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true} )
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// const wineSchema = new mongoose.Schema({
-//   wineName: {
-//     type: String,
-//     required: true
-//   },
-//   description: {
-//     type: String,
-//     required: true,
-//   },
-//   img: {
-//     data: Buffer,
-//     type: String,
-//     required: true
-//   },
-//
-// })
-//
-// let Wine = mongoose.model('Wine', wineSchema)
-//
-// const userSchema = new mongoose.Schema({
-//   userName: {
-//     type: String,
-//     required: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   }
-// })
-//
-// let User = mongoose.model('User', userSchema)
 
 app.get('/', (req, res) => {
   res.send('HEY!')
@@ -97,9 +33,5 @@ app.get('/test', (req, res) => {
   res.send('hello world')
 })
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(express.json())
-app.use('/', router)
+
 app.listen(PORT, () => { console.log(`started on ${PORT}`)})

f7064ebcbd2cc273e36b64cc95841b1ec4631804
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-18 08:51:45 +1000

    user add

--- index.js
+++ index.js
@@ -98,14 +98,14 @@ let User = mongoose.model('User', userSchema)
 
 // find all wines
 app.get('/user', (req, res) => {
-  res.send('hello world')
-  // Wine.find({},(err, response) => {
-  //   if (err) {
-  //     return console.log(err);
-  //   }
-  //   console.log("responseeeeee: ", response);
-  //   res.json(response);
-  // })
+
+  Wine.find({},(err, response) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("responseeeeee: ", response);
+    res.json(response);
+  })
 })
 
 app.get('/test', (req, res) => {

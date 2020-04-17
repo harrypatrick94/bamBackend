d4fbcc3d5890ffae8a6c759fea540e37cb4ab218
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-18 08:44:42 +1000

    added email funiction

--- index.js
+++ index.js
@@ -15,51 +15,43 @@ app.use(bodyParser.json())
 app.use(express.json())
 app.use('/', router)
 
-let transport = {
-    host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
-    port: 587,
-    auth: {
-    user: 'bensonandthemooch@gmail.com',
-    pass: 'discojuice'
-  }
-}
 
-let transporter = nodemailer.createTransport(transport)
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true} )
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+const wineSchema = new mongoose.Schema({
+  wineName: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  img: {
+    data: Buffer,
+    type: String,
+    required: true
+  },
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take messages');
-  }
-});
-
-router.post('/send', (req, res, next) => {
-  console.log(req.query);
-  let name = req.body.name
-  let email = req.body.email
-  let message = req.body.message
-  let content = `name: ${name} \n email: ${email} \n message: ${message} `
-
-  let mail = {
-    from: name,
-    to: 'bensonandthemooch@gmail.com',  // Change to email address that you want to receive messages on
-    subject: 'New Message from Contact Form',
-    text: content
-  }
-
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        status: err
-      })
-    } else {
-      res.json({
-       status: 'success'
-      })
-    }
-  })
 })
+
+let Wine = mongoose.model('Wine', wineSchema)
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+let User = mongoose.model('User', userSchema)
+
+
 
 app.get('/test', (req, res) => {
   res.send('hello world')

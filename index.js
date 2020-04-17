d2b1848915a50f2552f927b0778e8f39c8679dcb
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-18 08:49:20 +1000

    added email funiction

--- index.js
+++ index.js
@@ -15,6 +15,51 @@ app.use(bodyParser.json())
 app.use(express.json())
 app.use('/', router)
 
+let transport = {
+    host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
+    port: 587,
+    auth: {
+    user: 'bensonandthemooch@gmail.com',
+    pass: 'discojuice'
+  }
+}
+
+let transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+router.post('/send', (req, res, next) => {
+  console.log(req.query);
+  let name = req.body.name
+  let email = req.body.email
+  let message = req.body.message
+  let content = `name: ${name} \n email: ${email} \n message: ${message} `
+
+  let mail = {
+    from: name,
+    to: 'bensonandthemooch@gmail.com',  // Change to email address that you want to receive messages on
+    subject: 'New Message from Contact Form',
+    text: content
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: err
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
+    }
+  })
+})
 
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true} )
 const db = mongoose.connection;
@@ -51,7 +96,17 @@ const userSchema = new mongoose.Schema({
 
 let User = mongoose.model('User', userSchema)
 
+// find all wines
+app.get('/user', (req, res) => {
 
+  Wine.find({},(err, response) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("responseeeeee: ", response);
+    res.json(response);
+  })
+})
 
 app.get('/test', (req, res) => {
   res.send('hello world')

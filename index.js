0d721c19f5e39ab6fce273a14a8f8336a52c5935
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-18 15:01:34 +1000

    register

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.delete('/user/:name', (req, res) => {
 }) // delete single wine
 // create user
 app.post('/register', (req, res) => {
-  res.send(req.body)
+  res.send(req.query)
 //   const {userName, password} = req.body
 // //   // small validation
 //   if (!userName || !password) {

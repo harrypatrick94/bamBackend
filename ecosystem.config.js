c093662ee439624ab86decd0ba8e51c820c48ec6
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-29 12:17:37 +1000

    new sever

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: '~/.ssh/bensonMooch.pem',
       ref: 'origin/master',
       repo: 'git@github.com:harrypatrick94/bamBackend.git',
-      path: '/home/ubuntu/bamBackend/bamBackend',
+      path: '/home/ubuntu/bamBackend',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
